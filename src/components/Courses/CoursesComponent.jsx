fb2f5e28ea7aeb263effe1cf0c2e34924ad81147
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-11-30 04:44:07 +0000

    added course component images

--- src/components/Courses/CoursesComponent.jsx
+++ src/components/Courses/CoursesComponent.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import onlineCourse from '../../images/online.jpeg'
+import kidsCourse from '../../images/ninios.jpeg'
+import teensCourse from '../../images/adolescentes.jpeg'
+import './CoursesComponent.css'
 
 export default function CoursesComponent(){
+
     return (
         <>
-            <div className="courses pt-5 container-fluid" id='courses-section'>
+            <div className="courses pt-5 container-fluid mb-5" id='courses-section'>
                 <div className="container">
                     <div className='row'>
                         <h1>Cursos</h1>
                     </div>
-                    <div className='row align-items-center'>
-                        <div className='col-12 col-md-4'>curso  </div>
-                        <div className='col-12 col-md-4'>curso 3</div>
-                        <div className='col-12 col-md-4'> curso 2</div>
+                    <div className='row align-items-center imgs-container'>
+                        <div className='col-12 col-lg-4 mb-3'>
+                            <img className="course-img" src={onlineCourse} alt="" />
+                        </div>
+                        <div className='col-12 col-lg-4 mb-3'>
+                            <img className="course-img" src={kidsCourse} alt="" />
+                        </div>
+                        <div className='col-12 col-lg-4 mb-3'>
+                            <img className="course-img" src={teensCourse} alt="" />
+                        </div>
                     </div>
 
                 </div>
