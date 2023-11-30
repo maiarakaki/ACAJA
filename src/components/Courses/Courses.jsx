51b4a9973d2e2798f2bef7d6b001f4bf5a00e409
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-11-30 04:13:44 +0000

    created basic structure

---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './About.css';
+
+export default function About(){
+    return (
+        <>
+            <div className="courses pt-5 container-fluid" id='courses-section'>
+                <div className="container">
+                    <div className='row'>
+                        <h1>Cursos</h1>
+                    </div>
+                    <div className='row'>
+                        <div className='col'>curso  </div>
+                        <div className='col'>curso 3</div>
+                        <div className='col'> curso 2</div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+}
