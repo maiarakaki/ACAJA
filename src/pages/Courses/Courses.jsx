e94780ebf2fd8b049aa5f136435097c22b2546e5
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-11-30 13:50:28 +0000

    updated courses.jsx

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -3,6 +3,14 @@ import './Courses.css'
 import TopBanner from "../../components/TopBanner/TopBanner";
 import img from "../../images/pexels-kanji.jpg";
 
+function Children(){
+    return(
+        <div id="children-courses">
+            lalala
+        </div>
+    );
+}
+
 export default function Courses(){
     const bannerElements={
         title:"Nuestros Cursos", 
@@ -14,6 +22,7 @@ export default function Courses(){
     return (
         <>
             <TopBanner bannerElements={bannerElements} />
+            <Children />
         </>
     );
 }
