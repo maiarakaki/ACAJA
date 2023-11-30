0d5a4c1bd4440af10b0e08c022e8cf451877e6a2
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-11-30 03:28:22 +0000

    created hero banner

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactForm from "../components/ContactForm/ContactForm";
+import HeroBanner from "../components/HeroBanner/HeroBanner";
 
 export default function Landing(){
     return(
         <>
+            <HeroBanner />
             <ContactForm />
         </>
     );
