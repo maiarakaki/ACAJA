5abaf10603fc18a6c509428bb5e08b9076c8e0a9
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-11-30 03:21:42 +0000

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ContactForm from './components/ContactForm/ContactForm';
 import Landing from './pages/Landing';
 import Courses from './pages/Courses';
+import Events from './pages/Events';
+import FAQs from './pages/FAQs';
 import './App.css';
 
 
@@ -13,9 +15,20 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/courses" element={<Courses />} /> 
+        <Route path="/events" element={<Events />} /> 
+        <Route path="/faqs" element={<FAQs />} /> 
+        <Route path="*" element={<NotFound />} /> 
       </Routes>
     </BrowserRouter>
 
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h2>404</h2>
+    </div>
   );
 }
 
