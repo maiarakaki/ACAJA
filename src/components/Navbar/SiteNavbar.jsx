2724de290d9371bf810924df2f49689fa9d5ecff
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-11-30 19:55:34 +0000

    added navbar

--- src/components/Navbar/SiteNavbar.jsx
+++ src/components/Navbar/SiteNavbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from '../../images/logo-acaja.jpg'
 import SocialMedia from "../SocialMedia/SocialMedia";
 
@@ -7,9 +7,29 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 export default function SiteNavbar(){
+    const [iconColor] = useState("black");
     return(
         <>
-        NAVBAR PLACEHOLDER
+            <Navbar collapseOnSelect expand="lg" id="navbar">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img src={logo} width={80}/> ACAJA
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#about-us">Sobre Nosotros</Nav.Link>
+                        <Nav.Link href="#cursos">Cursos</Nav.Link>
+                        <Nav.Link href="#eventos">Eventos</Nav.Link>
+                        <Nav.Link href="#faqs">FAQs</Nav.Link>
+                        <Nav.Link href="#contacto">Contacto</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <SocialMedia iconColor={iconColor}/>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </>
     );
 }
