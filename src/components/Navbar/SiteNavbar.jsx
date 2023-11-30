e2b54076871870f81ef2c5124356d665c70d4629
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-11-30 19:48:55 +0000

    added navbar component to app

---
+++ src/components/Navbar/SiteNavbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from '../../images/logo-acaja.jpg'
+import SocialMedia from "../SocialMedia/SocialMedia";
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function SiteNavbar(){
+    return(
+        <>
+        NAVBAR PLACEHOLDER
+        </>
+    );
+}
