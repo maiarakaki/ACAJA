f0eaa1c4ad69c3afc74b7685bf434d20423cbed6
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-12-01 02:51:11 +0000

    created basic layout for events

--- src/components/EventComponent/EventsComponent.jsx
+++ src/components/EventComponent/EventsComponent.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import flyerImg from "../../images/bazaa-flyer.jpg"
 
 export default function EventsHome(){
     return(
         <>
-        
+            <div className="container-fluid events-container" id="events-section">
+                <div className="container">
+                    <div className="row">
+                        <h1>Novedades y eventos</h1>
+                        <div className="col-12 col-lg-6 spotlight-container">
+                            <img src={flyerImg}
+                                style={{objectFit:"contain",
+                                        width:"100%"}}/>
+                        </div>    
+                        <div className="col-12 col-lg6 gallery-container">
+                        </div>  
+                    </div>
+                    <div className="row container-fluid justify-content-center">
+                        <Link to='/events'>
+                            <button className="btn btn-primary">ver más</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
