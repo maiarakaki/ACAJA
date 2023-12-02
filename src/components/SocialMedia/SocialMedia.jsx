7a80e1fab8c5161c4e3e85dce24d760dd7144b4e
Maite Arakaki <67213065+maiarakaki@users.noreply.github.com>
2023-12-02 16:11:28 +0000

    updated styles

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -10,14 +10,16 @@ export default function SocialMedia({iconColor}){
                 <a href="https://www.facebook.com/profile.php?id=100070317800435/" 
                     target="_blank"
                     style={{color:{iconColor}}}>
-                    <FontAwesomeIcon icon={faFacebookSquare} style={{fontSize:"2rem", color: iconColor}} />
+                    <FontAwesomeIcon icon={faFacebookSquare} style={{fontSize:"2rem", color: iconColor}} 
+                        className="social-media-icon"/>
                 </a> 
             </div>
 
         
             <div className="col-6 social-media-icon-container" >
                 <a href="https://www.instagram.com/acaja.acassuso/" target="_blank" style={{color:{iconColor}}}>
-                    <FontAwesomeIcon icon={faInstagram}  style={{fontSize:"2rem", color: iconColor}}/>
+                    <FontAwesomeIcon icon={faInstagram}  style={{fontSize:"2rem", color: iconColor}}
+                        className="social-media-icon"/>
                 </a>
             </div>
 
